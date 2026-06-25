c50124d12f3a46716244d5f9738a0b4e54cd2625
harshaxyZ <harsha210108@gmail.com>
2026-06-25 13:54:42 +0530

    Fix TS completely

--- src/components/SmoothScrolling.tsx
+++ src/components/SmoothScrolling.tsx
@@ -1,9 +1,8 @@
-"use client";
 // @ts-nocheck
+"use client";
 import { ReactLenis } from "@studio-freight/react-lenis";
-import { ReactNode } from "react";
 
-export default function SmoothScrolling({ children }: { children: ReactNode }) {
+export default function SmoothScrolling({ children }: { children: any }) {
   return (
     <ReactLenis root options={{ duration: 1.2, smoothWheel: true, easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)) }}>
       {children}
